2c3dd2e9f9aa29f669ca7afa1ab1f5bae06c4aa9
eunbicho <puppy101976@naver.com>
2025-08-11 11:46:24 +0900

    Feat: 약관, 국가코드 상수 추가

---
+++ shinhan-reservation-app/src/constants/countryCodes.ts
@@ -0,0 +1,6 @@
+export const countryCodes = [
+  { label: "+82 (대한민국)", value: "82" },
+  { label: "+1 (미국)", value: "1" },
+  { label: "+81 (일본)", value: "81" },
+  { label: "+86 (중국)", value: "86" },
+];
