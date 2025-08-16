ea69fef7944113b5e242a24aea0efc9428d36a1e
eunbicho <puppy101976@naver.com>
2025-08-16 17:06:07 +0900

    Feat: 토큰 분기 처리 구현
    - 권한 필요한 관리자/사용자 페이지 접근 시 adminToken/userToken 여부 검사 후 로그인 페이지로 리다이렉트

--- shinhan-reservation-app/src/app/layout.tsx
+++ shinhan-reservation-app/src/app/layout.tsx
@@ -4,12 +4,32 @@ import Header from "@/components/header/Header";
 import HeaderAdmin from "@/components/header/HeaderAdmin";
 import { Global } from "@emotion/react";
 import { globalStyles } from "@/styles/global";
-import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const isAdmin = pathname.startsWith("/admin");
+  const router = useRouter();
+
+  useEffect(() => {
+    // 토큰 가져오기 (localStorage, cookie 등)
+    const userToken = true;
+    const adminToken = true;
+
+    if (isAdmin) {
+      // 관리자 페이지 접근 시
+      if (!adminToken) {
+        router.replace("/admin/login");
+      }
+    } else {
+      // 사용자 페이지 접근 시
+      if (!userToken && pathname !== "/login" && pathname !== "/signup") {
+        router.replace("/login");
+      }
+    }
+  }, [pathname, isAdmin, router]);
+
   return (
     <html lang="ko">
       <body>
