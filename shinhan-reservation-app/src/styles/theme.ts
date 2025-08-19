d81fd176920cb03dcc1bd5d961e6ee629cf302ec
eunbicho <puppy101976@naver.com>
2025-08-19 16:53:45 +0900

    Style: 색상변수 graycolor30 추가

--- shinhan-reservation-app/src/styles/theme.ts
+++ shinhan-reservation-app/src/styles/theme.ts
@@ -3,6 +3,7 @@ const colors = {
   graycolor50: "#8C8F93",
   graycolor20: "#D1D2D4",
   graycolor10: "#E8E9E9",
+  graycolor30: "#BABCBE",
   graycolor5: "#F3F4F4",
   graycolor0: "#FFFFFF",
   maincolor: "#0046FF",
