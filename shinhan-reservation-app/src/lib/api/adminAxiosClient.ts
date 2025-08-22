660363398934bd738df43965080a2104297c44c9
eunbicho <puppy101976@naver.com>
2025-08-22 11:58:29 +0900

    Feat: 사용자/관리자 axios 설정
    - 로그인이나 회원가입 페이지에서는 refresh 시도 막기 (무한 요청 이슈 해결)

---
+++ shinhan-reservation-app/src/lib/api/adminAxiosClient.ts
@@ -0,0 +1,49 @@
+// src/lib/adminAxiosClient.ts
+import axios from "axios";
+import { useAdminAuthStore } from "@/store/adminAuthStore";
+
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const adminAxiosClient = axios.create({
+  baseURL: baseURL,
+});
+
+adminAxiosClient.interceptors.request.use((config) => {
+  const token = useAdminAuthStore.getState().adminAccessToken;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+adminAxiosClient.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // 토큰 만료 → refresh 시도
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/admin/auth/login") &&
+      !originalRequest.url.includes("/admin/signup")
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const { data } = await adminAxiosClient.post("/api/admin/auth/refresh");
+
+        useAdminAuthStore.getState().setAdminAccessToken(data.accessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+        return adminAxiosClient(originalRequest);
+      } catch (err) {
+        useAdminAuthStore.getState().clearAdminAuth();
+        window.location.href = "/admin/login"; // 관리자 로그인 페이지로 리다이렉트
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default adminAxiosClient;
--- shinhan-reservation-app/src/lib/api/axiosClient.ts
+++ shinhan-reservation-app/src/lib/api/axiosClient.ts
@@ -1,14 +1,14 @@
 // src/lib/axiosClient.ts
 import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const router = useRouter();
+// const router = useRouter();
 
 const axiosClient = axios.create({
   baseURL: baseURL,
-  withCredentials: true, // refresh token 쿠키 포함
+  //withCredentials: true, // refresh token 쿠키 포함
 });
 
 axiosClient.interceptors.request.use((config) => {
@@ -25,17 +25,24 @@ axiosClient.interceptors.response.use(
     const originalRequest = error.config;
 
     // 토큰 만료 시 refresh 시도
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/login") &&
+      !originalRequest.url.includes("/users/signup") &&
+      !originalRequest.url.includes("/sms/")
+    ) {
       originalRequest._retry = true;
 
       try {
-        const { data } = await axiosClient.post("/auth/refresh");
+        const { data } = await axiosClient.post("/api/auth/refresh");
         useAuthStore.getState().setAccessToken(data.accessToken);
         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
         return axiosClient(originalRequest);
       } catch (err) {
         useAuthStore.getState().clearAuth();
-        router.push("/login");
+        // router.push("/login");
+        window.location.href = "/login";
       }
     }
     return Promise.reject(error);
