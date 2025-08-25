2cd5f95a0b75c3cb1ade886fee80b852100194f3
eunbicho <puppy101976@naver.com>
2025-08-26 04:20:20 +0900

    Feat: 캘린더 모달 컴포넌트 UI 개발

--- shinhan-reservation-app/src/app/admin/space/components/CalendarModal.tsx
+++ shinhan-reservation-app/src/app/admin/space/components/CalendarModal.tsx
@@ -7,8 +7,13 @@ import {
   Overlay,
   Header,
   CloseButton,
+  GapBox,
+  Footer,
 } from "./SpaceFormModal/styles";
 import { IoCloseOutline } from "react-icons/io5";
+import styled from "@emotion/styled";
+import colors from "@/styles/theme";
+import Button from "@/components/common/button/Button";
 
 interface Props {
   isOpen: boolean;
@@ -26,17 +31,71 @@ const CalendarModal: React.FC<Props> = ({ isOpen, onClose, onSelect }) => {
         aria-modal="true"
         aria-labelledby="sfm-title"
       >
-        {/* 1. Header */}
         <Header>
           <h2 id="sfm-title">하루 또는 기간 선택</h2>
           <CloseButton onClick={onClose} aria-label="닫기">
             <IoCloseOutline size={26} />
           </CloseButton>
         </Header>
-        <Calendar />
+        <Calendar onSelectDate={(result) => console.log(result)} />
+
+        <InfoWrapper>
+          <InfoBox>
+            <Info color={colors.graycolor20}>
+              <div></div>
+              <p>오늘</p>
+            </Info>
+            <Info color={colors.maincolor}>
+              <div></div>
+              <p>선택</p>
+            </Info>
+          </InfoBox>
+        </InfoWrapper>
+        <GapBox h="1.94rem" />
+        <Footer>
+          <Button onClick={() => {}} isActive={true}>
+            휴무일로 등록하기
+          </Button>
+        </Footer>
       </ModalContainer>
     </Overlay>
   );
 };
 
 export default CalendarModal;
+
+const InfoWrapper = styled.div`
+  margin-top: 0.75rem;
+  height: 2.625rem;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const InfoBox = styled.div`
+  display: flex; /* Info들을 가로 배치 */
+  gap: 1rem; /* Info끼리 간격 */
+  border-top: 1px solid ${colors.graycolor5};
+  width: 22.9rem;
+  padding-top: 1rem;
+  padding-bottom: 0.5rem;
+`;
+
+const Info = styled.div<{ color: string }>`
+  display: flex; /* 동그라미 + 텍스트 가로 배치 */
+  align-items: center;
+  gap: 0.22rem; /* 동그라미와 글자 간격 */
+
+  div {
+    width: 0.68rem;
+    height: 0.68rem;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+    flex-shrink: 0;
+  }
+
+  p {
+    font-size: 0.875rem;
+    color: ${colors.graycolor100};
+  }
+`;
