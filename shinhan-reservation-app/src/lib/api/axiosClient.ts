c01841f7caffe457d2c7f08b43b9be97e0c3642a
eunbicho <puppy101976@naver.com>
2025-08-28 21:31:57 +0900

    Feat: qs로 paramsSerializer 추가 및 로그아웃 로직 추가
    - refresh 대상이 아닌 401은 바로 로그아웃되도록 구현

--- shinhan-reservation-app/src/lib/api/axiosClient.ts
+++ shinhan-reservation-app/src/lib/api/axiosClient.ts
@@ -1,6 +1,8 @@
 // src/lib/axiosClient.ts
 import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
+import qs from "qs";
+
 // import { useRouter } from "next/router";
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
@@ -8,6 +10,7 @@ const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const axiosClient = axios.create({
   baseURL: baseURL,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
   //withCredentials: true, // refresh token 쿠키 포함
 });
 
@@ -24,27 +27,37 @@ axiosClient.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    // 토큰 만료 시 refresh 시도
+    // refresh 대상이 아닌 401 → 바로 로그아웃
     if (
       error.response?.status === 401 &&
-      !originalRequest._retry &&
       !originalRequest.url.includes("/auth/login") &&
       !originalRequest.url.includes("/users/signup") &&
-      !originalRequest.url.includes("/sms/")
+      !originalRequest.url.includes("/users/logout") &&
+      !originalRequest.url.includes("/sms/") &&
+      !originalRequest.url.includes("/auth/refresh") // refresh 요청 자체도 제외
     ) {
-      originalRequest._retry = true;
+      if (!originalRequest._retry) {
+        originalRequest._retry = true;
 
-      try {
-        const { data } = await axiosClient.post("/api/auth/refresh");
-        useAuthStore.getState().setAccessToken(data.accessToken);
-        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
-        return axiosClient(originalRequest);
-      } catch (err) {
+        try {
+          // refresh 시도
+          const { data } = await axiosClient.post("/api/auth/refresh");
+          useAuthStore.getState().setAccessToken(data.accessToken);
+          originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+          return axiosClient(originalRequest);
+        } catch (err) {
+          // refresh 실패 → 로그아웃
+          useAuthStore.getState().clearAuth();
+          window.location.href = "/login";
+          return Promise.reject(err);
+        }
+      } else {
+        // 이미 refresh 시도한 경우 → 그냥 로그아웃
         useAuthStore.getState().clearAuth();
-        // router.push("/login");
         window.location.href = "/login";
       }
     }
+
     return Promise.reject(error);
   }
 );
